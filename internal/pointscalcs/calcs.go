34fd1a249ee971fbff9582395990019442148395
Manuel Carbajal <manicar2093@gmail.com>
2023-09-21 01:04:50 -0600

    feat: add all points calculators

--- internal/pointscalcs/calcs.go
+++ internal/pointscalcs/calcs.go
@@ -2,8 +2,11 @@ package pointscalcs
 
 import (
 	"math"
+	"strings"
+	"time"
 
 	"github.com/manicar2093/fetch-receipt-processor-challenge/internal/receipts"
+	"github.com/manicar2093/fetch-receipt-processor-challenge/pkg/logger"
 )
 
 func ByRetailerName(r receipts.Receipt) int {
@@ -25,6 +28,55 @@ func ByTotalMultipleOf25(r receipts.Receipt) int {
 	)
 	if isInteger(divRes) {
 		points = 25
+	}
+	return points
+}
+
+func ByEach2Items(r receipts.Receipt) int {
+	var (
+		itemsLen        = float64(len(r.Items)) / 2
+		itemsLenFloored = math.Floor(itemsLen)
+	)
+
+	return int(itemsLenFloored) * 5
+}
+
+func ByItemTrimmedDescription(r receipts.Receipt) int {
+	var points int
+
+	for _, item := range r.Items {
+		trimmedDescription := strings.Trim(item.ShortDescription, " ")
+		if len(trimmedDescription)%3 != 0 {
+			continue
+		}
+		points += int(math.Ceil(item.Price * .2))
+	}
+
+	return points
+}
+
+func ByPurchaseDayIsOdd(r receipts.Receipt) int {
+	points := 0
+	if r.PurchaseDate.Day()%2 != 0 {
+		points = 6
+	}
+	return points
+}
+
+func ByPurchaseTwoFourInterval(receipt receipts.Receipt) int {
+	var (
+		timeFormat = "15:04"
+		initTime   = time.Date(0, time.January, 1, 14, 0, 0, 0, time.UTC)
+		finalTime  = time.Date(0, time.January, 1, 16, 0, 0, 0, time.UTC)
+		points     = 0
+	)
+	t, err := time.Parse(timeFormat, receipt.PurchaseTime)
+	if err != nil {
+		logger.GetLogger().WithField("error", err).Error("returning 0 as default value")
+		return points
+	}
+	if t.After(initTime) && t.Before(finalTime) {
+		points = 10
 	}
 	return points
 }
--- internal/pointscalcs/calcs_test.go
+++ internal/pointscalcs/calcs_test.go
@@ -1,6 +1,8 @@
 package pointscalcs_test
 
 import (
+	"time"
+
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 
@@ -57,25 +59,73 @@ var _ = Describe("Calcs", func() {
 
 	Describe("ByEach2Items", func() {
 		It("5 points for every two items on the receipt", func() {
+			var (
+				expectedReceipt = receipts.Receipt{
+					Items: []receipts.ReceiptItem{
+						{},
+						{},
+						{},
+						{},
+						{},
+					},
+				}
+				expectedPoints = 10
+			)
 
+			got := pointscalcs.ByEach2Items(expectedReceipt)
+
+			Expect(got).To(Equal(expectedPoints))
 		})
 	})
 
-	Describe("ByItemtrimmedDescription", func() {
-		It("trimmes item description, vaidates is multiple of 3, multiply it by 3 and rounde it up to the nearest integer", func() {
+	Describe("ByItemTrimmedDescription", func() {
+		It("trimmes item description, vaidates is multiple of 3, multiply it by 3 and round it up to the nearest integer", func() {
+			var (
+				expectedReceipt = receipts.Receipt{
+					Items: []receipts.ReceiptItem{
+						{ShortDescription: "Mountain Dew 12PK", Price: 6.49},
+						{ShortDescription: "Emils Cheese Pizza", Price: 12.25},
+						{ShortDescription: "Knorr Creamy Chicken", Price: 1.26},
+						{ShortDescription: "Doritos Nacho Cheese", Price: 3.35},
+						{ShortDescription: "   Klarbrunn 12-PK 12 FL OZ  ", Price: 12.00},
+					},
+				}
+				expectedPoints = 6
+			)
 
+			got := pointscalcs.ByItemTrimmedDescription(expectedReceipt)
+
+			Expect(got).To(Equal(expectedPoints))
 		})
 	})
 
 	Describe("ByPurchaseDayIsOdd", func() {
 		It("gives 6 points if the day in the purchase date is odd", func() {
+			var (
+				expectedReceipt = receipts.Receipt{
+					PurchaseDate: time.Date(2022, time.January, 1, 0, 0, 0, 0, time.Local),
+				}
+				expectedPoints = 6
+			)
 
+			got := pointscalcs.ByPurchaseDayIsOdd(expectedReceipt)
+
+			Expect(got).To(Equal(expectedPoints))
 		})
 	})
 
 	Describe("ByPurchaseTwoFourInterval", func() {
 		It("gives 10 points if the time of purchase is after 2:00pm and before 4:00pm", func() {
+			var (
+				expectedReceipt = receipts.Receipt{
+					PurchaseTime: "14:33",
+				}
+				expectedPoints = 10
+			)
 
+			got := pointscalcs.ByPurchaseTwoFourInterval(expectedReceipt)
+
+			Expect(got).To(Equal(expectedPoints))
 		})
 	})
 })
